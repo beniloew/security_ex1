d62d89ff38473281d8688dde25cff6aadf7371ef
Beni Lev <beniloew@gmail.com>
2013-03-04 00:02:44 +0200

    first prime implementation
    Signed-off-by: Beni Lev <beniloew@gmail.com>

--- carmichael.asm
+++ carmichael.asm
@@ -12,3 +12,36 @@ calc_mod:
 
 calc_mod_exit:
 	ret
+
+; TODO - check if ax is 1 or 0 ;
+
+is_prime:
+	push bx
+	push cx
+	cmp ax,1
+	jbe is_not_prime
+	mov cx,ax ; save the original ax, because ax will be used as argument and ret value for calc_mod
+	mov bx,ax ; bx - the counter, counts downwise
+	sar bx,1 ; divide the counter by 2 - bigger numbers than that are'nt relevant
+is_prime_loop:
+	cmp bx,1
+	je is_prime_indeed
+	mov ax,cx
+	call calc_mod
+	cmp ax,0
+	jne is_prime_exit ; ax is 0 - that means that the original ax isn't a prime
+	dec bx
+	jmp is_prime_loop
+
+is_prime_exit:
+	pop cx
+	pop bx
+	ret
+
+is_prime_indeed:
+	mov ax,1
+	jmp is_prime_exit
+
+is_not_prime:
+	mov ax,0
+	jmp is_prime_exit
