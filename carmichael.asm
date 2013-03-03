6441e5f8695516d1a90102651c099b977b28bba1
Beni Lev <beniloew@gmail.com>
2013-03-03 23:04:24 +0200

    calc_mod implementation
    Signed-off-by: Beni Lev <beniloew@gmail.com>

--- carmichael.asm
+++ carmichael.asm
@@ -1,22 +1,14 @@
 org 100h
 
-mov ax,3
-mov bx,4
-
-jmp calc_mod
-mov dx,ax
-mov ah,9
-int 21h
+call calc_mod
 mov ah,4Ch
 int 21h
 
 calc_mod:
 	cmp ax,bx ; if ax < bx then return
 	jb calc_mod_exit
-
-calc_mod_loop:
 	sub ax,bx
-	jae calc_mod_loop
+	jmp calc_mod
 
 calc_mod_exit:
 	ret
